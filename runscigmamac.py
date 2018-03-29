86b602fafce8de7f6f97355c873c5b2c0db764c6
Yorgos Ps <george_psar@hotmail.com>
2018-03-29 12:01:02 -0400

    Create runscigmamac.py

---
+++ runscigmamac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+try:
+    import scigma
+except:
+    import traceback
+    traceback.print_exc()
+    
+    print()
+    print("Could not import scigma.")
+raw_input("Press enter to exit.")
+
+print("Exiting.")
